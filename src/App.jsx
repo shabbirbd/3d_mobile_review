e48e309cd26ba8a4677ef7a2bab0138c042a79db
shabbirbd <shabbirhossainshah@gmail.com>
2024-03-20 19:49:20 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
 
 const App = () => {
 
   return (
     <>
-      <h1>hello world</h1>       
+      <main className="bg-black">
+        <Navbar/>
+        <Hero/>
+        <Highlights/>  
+      </main>    
     </>
   )
 }
